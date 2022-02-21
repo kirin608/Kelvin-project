78c70229c350ff074e5b98ac6e642161aa9b40a2
Andrew Shapiro <77997602+kirin608@users.noreply.github.com>
2022-02-21 17:19:59 -0500

    Create dogYears.js

---
+++ dogYears.js
@@ -0,0 +1,16 @@
+var myAge = 34;
+// my age
+var earlyYears = 2;
+//early years
+earlyYears= earlyYears * 10.5;
+var laterYears = myAge -2 ;
+//step 4
+
+
+
+console.log(myAge, laterYears);
+
+var myAgeInDogYears = laterYears * 4 + earlyYears;
+console.log(`my age of ${myAge} in dog years equates to ${myAgeInDogYears} `);
+const myName = "Andrew";
+console.log(`my name is ${myName.toLowerCase()}. I am ${myAge}years old in human years which is ${myAgeInDogYears} years old in dog years  `);
